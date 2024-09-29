bebbb5f7812ade357416f12a8f602779803e9bbe
ahmed mahmoud <ahmad.mahmoud1472003@gmail.com>
2024-09-29 08:46:16 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -9,19 +9,23 @@ const todayImg = document.getElementById("todayImg");
 const tomImg = document.getElementById("tomImg");
 const afterImg = document.getElementById("afterImg");
 const tomDayName = document.getElementById("tomDayName");
+const afterTomName = document.getElementById("afterTomName");
 const tom_max_temp = document.getElementById("tom_max_temp");
 const tom_min_temp = document.getElementById("tom_min_temp");
-const afterTomName = document.getElementById("afterTomName");
 const after_tom_max_temp = document.getElementById("after_tom_max_temp");
 const after_tom_min_temp = document.getElementById("after_tom_min_temp");
 const todaySpan = document.getElementById("todaySpan");
 const tomSpan = document.getElementById("tomSpan");
 const afterSpan = document.getElementById("afterSpan");
+const searchInput = document.getElementById("searchInput");
+// let DATE = new Date();
+// console.log(DATE.toLocaleDateString("en-UK", { weekday: "long" }));
+// console.log(DATE.getDate());
 
-async function getWeatherData(country) {
+async function getWeatherData(cityName) {
   try {
     let res = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=afbef66c5eb74ca5885120414242206&q=cairo&days=3`,
+      `https://api.weatherapi.com/v1/forecast.json?key=afbef66c5eb74ca5885120414242206&q=${cityName}&days=3`,
       { method: "GET" }
     );
     let weatherData = await res.json();
@@ -31,29 +35,54 @@ async function getWeatherData(country) {
   }
 }
 function displayTodayData(data) {
+  let todayDate = new Date();
   countryName.innerHTML = data.location.name;
   rain.innerHTML = data.current.humidity + "%";
   temp.innerHTML = data.current.temp_c;
   wind.innerHTML = data.current.wind_kph + "km/h";
-//   todayImg.setAttribute("src", data.current.condition.icon);
+  //   todayImg.setAttribute("src", data.current.condition.icon);
   todaySpan.innerHTML = data.current.condition.text;
+  dayName.innerHTML = todayDate.toLocaleDateString("en-UK", {
+    weekday: "long",
+  });
+  date.innerHTML =
+    todayDate.getDate() +
+    todayDate.toLocaleDateString("en-UK", { month: "long" });
 }
 function displayTomData(data) {
   let forecastData = data.forecast.forecastday;
+  let tomDate = new Date(forecastData[1].date);
+  let afterDate = new Date(forecastData[2].date);
+  // console.log(tomDate);
+  // console.log(afterDate);
+
   tomDayName.innerHTML = forecastData[1].date;
   tom_max_temp.innerHTML = forecastData[1].day.maxtemp_c;
   tom_min_temp.innerHTML = forecastData[1].day.mintemp_c;
-//   tomImg.setAttribute("src", forecastData[1].day.condition.icon);
+  //   tomImg.setAttribute("src", forecastData[1].day.condition.icon);
   tomSpan.innerHTML = forecastData[1].day.condition.text;
+  tomDayName.innerHTML = tomDate.toLocaleDateString("en-UK", {
+    weekday: "long",
+  });
+  afterTomName.innerHTML = forecastData[2].date;
   after_tom_max_temp.innerHTML = forecastData[2].day.maxtemp_c;
   after_tom_min_temp.innerHTML = forecastData[2].day.mintemp_c;
-//   afterImg.setAttribute("src", forecastData[2].day.condition.icon);
+  //   afterImg.setAttribute("src", forecastData[2].day.condition.icon);
   afterSpan.innerHTML = forecastData[2].day.condition.text;
-//   console.log(forecastData[2].day.condition.text);
+  //   console.log(forecastData[2].day.condition.text);
+  afterTomName.innerHTML = afterDate.toLocaleDateString("en-UK", {
+    weekday: "long",
+  });
 }
-async function srartApp() {
-  let weatherData = await getWeatherData();
-  displayTodayData(weatherData);
-  displayTomData(weatherData);
+async function srartApp(city = "cairo") {
+  let weatherData = await getWeatherData(city);
+  if (!weatherData.error) {
+    displayTodayData(weatherData);
+    displayTomData(weatherData);
+  }
 }
 srartApp();
+searchInput.addEventListener("input", function () {
+  srartApp(searchInput.value);
+  // console.log(searchInput.value);
+});
